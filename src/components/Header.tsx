515357678bf1296e42e30d820e22d83d3e9aeacd
jhansarmiento <jhancsn1@gmail.com>
2025-06-16 23:54:58 -0500

    add Navlink to handle navigation between pages while maintaining good performance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <>
       <div className="bg-gray-950">
-        <div className="mx-auto px-12 py-6">
+        <div className="mx-auto container px-4 py-6">
             <div className="flex justify-between items-center">
+
                 <img src="/logo.svg" alt="" className="w-16"/>
+
+                <nav className="flex px-4 gap-6">
+                    <NavLink 
+                        to='/'
+                        className={({isActive}) =>
+                        isActive ? 'text-orange-500 font-bold uppercase' : 'text-white font-bold uppercase'}
+                    >Home
+                    </NavLink>
+                    <NavLink
+                        to='/favorites'
+                        className={({isActive}) =>
+                        isActive ? 'text-orange-500 font-bold uppercase' : 'text-white font-bold uppercase'}  
+                    >Favorites</NavLink>
+                </nav>
             </div>
 
-            <nav>
-
-            </nav>
         </div>
       </div>
     </>
