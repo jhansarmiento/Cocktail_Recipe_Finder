36d6d8af19026fa98238a325a187e3ef326b7b2d
jhansarmiento <jhancsn1@gmail.com>
2025-06-16 22:56:47 -0500

    creates the router and the beginnings of the header component

---
+++ src/AppRouter.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import IndexPage from "./views/IndexPage"
+import FavoritesPage from "./views/FavoritesPage"
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<IndexPage />} />
+          <Route path='/favorites' element={<FavoritesPage />} /> 
+        </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+
+export default function Header() {
+  return (
+    <>
+      <div className="bg-gray-950">
+        <div className="mx-auto px-12 py-6">
+            <div className="flex justify-between items-center">
+                <img src="/logo.svg" alt="" className="w-18"/>
+            </div>
+
+            <nav>
+                
+            </nav>
+        </div>
+      </div>
+    </>
+  )
+}
